501fbb9d502af557147069c44f72a6dbf230e237
Rafi N Kusuma <rafiuniverse2@gmail.com>
2023-11-22 20:56:11 +0700

    ADDED Jdbc

--- src/main/java/com/smk/view/CreateBooking.java
+++ src/main/java/com/smk/view/CreateBooking.java
@@ -1,11 +1,13 @@
 package com.smk.view;
 
 import com.smk.MainView;
+import com.smk.dao.LocationDao;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Location;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -16,13 +18,26 @@ import com.vaadin.flow.router.Route;
 
 public class CreateBooking extends VerticalLayout {
 
+    private LocationDao locationDao;
+
     public CreateBooking(){
+        locationDao = new LocationDao();
         createForm();
     }
+
+
             private void createForm(){
                 setAlignItems(Alignment.STRETCH);
-                ComboBox fromComboBox = new ComboBox("Dari");
-                ComboBox toComboBox = new ComboBox("Ke");
+
+                ComboBox<Location> fromComboBox = new ComboBox("Dari");
+                fromComboBox.setItems(locationDao.getAll());
+                fromComboBox.setItemLabelGenerator(Location::getName);
+
+
+                ComboBox<Location> toComboBox = new ComboBox("Ke");
+                toComboBox.setItems(locationDao.getAll());
+                toComboBox.setItemLabelGenerator(Location::getName);
+
                 DatePicker departureDatePicker = new DatePicker("Tanggal Keberangkatan");
                 DatePicker arrivalDatePicker = new DatePicker("Tanggal Kedatangan");
                 Button searchButton = new Button("Search");
@@ -30,5 +45,6 @@ public class CreateBooking extends VerticalLayout {
 
                 add(fromComboBox,toComboBox,departureDatePicker,arrivalDatePicker,searchButton);
             }
+
 }
 
